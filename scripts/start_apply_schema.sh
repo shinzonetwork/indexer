bd3409e5d1a090466499aa213637cd674bd0cb19
dvncan <44993570+dvncan@users.noreply.github.com>
2025-07-23 12:33:35 -0400

    chore: remove redundant start command from apply_schema (#31)

--- scripts/start_apply_schema.sh
+++ scripts/start_apply_schema.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# Start Defra with rootdir
-~/go/bin/defradb start --rootdir ~/Developer/shinzo/version1/.defra & 
-
 # Apply the new schema
 ~/go/bin/defradb client schema add -f schema/schema.graphql
