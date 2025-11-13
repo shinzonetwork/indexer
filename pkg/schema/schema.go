0357f2a1f9041f330e9ef4c52add24bd75f002f4
Quinn Purdy <quinn.purdy.gamedev@gmail.com>
2025-11-13 15:53:06 -0500

    Fix comment

--- pkg/schema/schema.go
+++ pkg/schema/schema.go
@@ -7,11 +7,7 @@ import (
 //go:embed schema.graphql
 var SchemaGraphQL string
 
-// GetSchema returns the GraphQL schema as a string.
-// This is the source of truth for the schema used by both indexer and host.
-// Note: The actual schema file is maintained in the root schema/ directory,
-// and this file is a copy for embedding purposes.
+// GetSchema returns the GraphQL schema found in `schema.graphql` as a string.
 func GetSchema() string {
 	return SchemaGraphQL
 }
-
